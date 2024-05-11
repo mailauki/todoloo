7f60fa14d78bfc175551c408e1689fc37b73f94e
Julie Evans <juliemevans17@gmail.com>
2024-05-10 19:24:41 -0500

    Update open context for profile

--- app/account/account.tsx
+++ app/account/account.tsx
@@ -10,16 +10,23 @@ import AvatarDL from './avatar-dl';
 import { createClient } from '@/utils/supabase/client';
 import { updateProfile } from './actions';
 import AvatarForm from './avatar-form';
+import { useOpen } from '@/utils/context';
+import ColorPicker from '../components/color-picker';
 
-export default function Account({ user, serverProfile }: { user: User | null, serverProfile: Profile }) {
+export default function Account({
+	user, serverProfile,
+}: {
+	user: User | null,
+	serverProfile: Profile,
+}) {
 	const [profile, setProfile] = React.useState(serverProfile);
 	// const {full_name, username, avatar_url, color} = profile;
 	const {username, avatar_url} = profile;
-	const [openUpdate, setOpenUpdate] = React.useState(false);
-	const [openColor, setOpenColor] = React.useState(false);
+	const { openProfileUpdate, setOpenProfileUpdate, openProfileColor, setOpenProfileColor } = useOpen();
+	// const [openUpdate, setOpenUpdate] = React.useState(false);
+	// const [openColor, setOpenColor] = React.useState(false);
 	const supabase = createClient();
 
-	console.log(openColor);
 	// console.log({user});
 	// console.log({profile});
 	// console.log({full_name}, {username}, {avatar_url}, {color});
@@ -29,20 +36,20 @@ export default function Account({ user, serverProfile }: { user: User | null, se
   // };
 
 	const handleOpenUpdate = () => {
-    setOpenUpdate(true);
+    setOpenProfileUpdate(true);
   };
 
   const handleCloseUpdate = () => {
-    setOpenUpdate(false);
+    setOpenProfileUpdate(false);
   };
 
 	const handleOpenColor = () => {
-    setOpenColor(true);
+    setOpenProfileColor(true);
   };
 
-  // const handleCloseColor = () => {
-  //   setOpenColor(false);
-  // };
+  const handleCloseColor = () => {
+    setOpenProfileColor(false);
+  };
 
 	React.useEffect(() => {
 		const channel = supabase.channel('realtime profile')
@@ -58,143 +65,193 @@ export default function Account({ user, serverProfile }: { user: User | null, se
 		};
 	});
 
+	function Profile() {
+		return (
+			<Stack spacing={2}>
+				<Stack
+					alignItems='center'
+					direction='row'
+					justifyContent='center'
+				>
+					<Paper
+						elevation={0}
+						sx={{
+							width: 'fit-content',
+							borderRadius: '50%',
+							p: 0.5,
+						}}
+					>
+						<AvatarDL url={avatar_url} />
+					</Paper>
+				</Stack>
+
+				<Card
+					elevation={0}
+					sx={{
+						backgroundColor: 'card.paper',
+					}}
+				>
+					<CardHeader
+						sx={{ textAlign: 'center' }}
+						title={username! || user?.email}
+					/>
+					<List
+						component={CardContent}
+						disablePadding
+					>
+						<ListItem
+							secondaryAction={
+								<Typography
+									color='text.secondary'
+									variant='subtitle2'
+								>
+									{user?.email}
+								</Typography>
+							}
+						>
+							<ListItemIcon>
+								<Mail />
+							</ListItemIcon>
+							<ListItemText primary='Email' />
+						</ListItem>
+
+						<ListItem
+							disablePadding
+							secondaryAction={
+								<ChevronRight />
+							}
+						>
+							<ListItemButton onClick={handleOpenUpdate}>
+								<ListItemIcon>
+									<Edit />
+								</ListItemIcon>
+								<ListItemText primary='Update Profile' />
+							</ListItemButton>
+						</ListItem>
+
+						<ListItem
+							disablePadding
+							secondaryAction={
+								<ChevronRight />
+							}
+						>
+							<ListItemButton onClick={handleOpenColor}>
+								<ListItemIcon>
+									<Palette />
+								</ListItemIcon>
+								<ListItemText primary='Color' />
+							</ListItemButton>
+						</ListItem>
+
+						<Divider sx={{ my: 2 }} />
+
+						<ListItem
+							action='/auth/signout'
+							component='form'
+							disablePadding
+							method='post'
+						>
+							<ListItemButton component='button' type='submit'>
+								<ListItemIcon>
+									<Logout />
+								</ListItemIcon>
+								<ListItemText primary='Signout' />
+							</ListItemButton>
+						</ListItem>
+					</List>
+			</Card>
+		</Stack>
+		);
+	}
+
+	function UpdateProfile() {
+		return (
+			<Stack spacing={2}>
+				<Stack
+					alignItems='center'
+					direction='row'
+					justifyContent='center'
+				>
+					<AvatarForm
+						onUpload={(url: string) => {
+							console.log(url);
+							// setAvatarUrl(url);
+							updateProfile({ full_name: profile.full_name, username: profile.username, avatar_url: url, color: profile.color });
+						}}
+						uid={user?.id ?? null}
+						url={avatar_url}
+					/>
+				</Stack>
+				<Card
+					elevation={0}
+					sx={{
+						backgroundColor: 'card.paper',
+					}}
+				>
+					<CardHeader
+						sx={{ textAlign: 'center' }}
+						title={username! || user?.email}
+					/>
+					<CardContent>
+						<Button
+							color='info'
+							onClick={handleCloseUpdate}
+							sx={{ mb: 2 }}
+						>
+							Close
+						</Button>
+						<AccountForm profile={profile} user={user} />
+					</CardContent>
+				</Card>
+			</Stack>
+		);
+	}
+
+	function UpdateColor() {
+		return (
+			<Stack spacing={2}>
+				<Card
+					elevation={0}
+					sx={{
+						backgroundColor: 'card.paper',
+					}}
+				>
+					{/* <CardHeader
+						sx={{ textAlign: 'center' }}
+						title={username! || user?.email}
+					/> */}
+					<CardContent>
+						<Button
+							color='info'
+							onClick={handleCloseColor}
+							sx={{ mb: 2 }}
+						>
+							Close
+						</Button>
+						<ColorPicker
+							color={profile.color}
+							onColorChange={(color_code) => {
+								console.log(color_code);
+								updateProfile({ full_name: profile.full_name, username: profile.username, avatar_url: profile.avatar_url, color: color_code });
+							}}
+						/>
+					</CardContent>
+				</Card>
+			</Stack>
+		);
+	}
+
 	return (
 		<>
 			<Main>
-				{!openUpdate ? (
-					<Stack spacing={2}>
-						<Stack
-							alignItems='center'
-							direction='row'
-							justifyContent='center'
-						>
-							<Paper
-								elevation={0}
-								sx={{
-									width: 'fit-content',
-									borderRadius: '50%',
-									p: 0.5,
-								}}
-							>
-								<AvatarDL url={avatar_url} />
-							</Paper>
-						</Stack>
-
-						<Card
-							elevation={0}
-							sx={{
-								backgroundColor: 'card.paper',
-							}}
-						>
-							<CardHeader
-								sx={{ textAlign: 'center' }}
-								title={username! || user?.email}
-							/>
-							<List
-								component={CardContent}
-								disablePadding
-							>
-								<ListItem
-									secondaryAction={
-										<Typography
-											color='text.secondary'
-											variant='subtitle2'
-										>
-											{user?.email}
-										</Typography>
-									}
-								>
-									<ListItemIcon>
-										<Mail />
-									</ListItemIcon>
-									<ListItemText primary='Email' />
-								</ListItem>
-
-								<ListItem
-									disablePadding
-									secondaryAction={
-										<ChevronRight />
-									}
-								>
-									<ListItemButton onClick={handleOpenUpdate}>
-										<ListItemIcon>
-											<Edit />
-										</ListItemIcon>
-										<ListItemText primary='Update Profile' />
-									</ListItemButton>
-								</ListItem>
-
-								<ListItem
-									disablePadding
-									secondaryAction={
-										<ChevronRight />
-									}
-								>
-									<ListItemButton onClick={handleOpenColor}>
-										<ListItemIcon>
-											<Palette />
-										</ListItemIcon>
-										<ListItemText primary='Color' />
-									</ListItemButton>
-								</ListItem>
-
-								<Divider sx={{ my: 2 }} />
-
-								<ListItem
-									action='/auth/signout'
-									component='form'
-									disablePadding
-									method='post'
-								>
-									<ListItemButton component='button' type='submit'>
-										<ListItemIcon>
-											<Logout />
-										</ListItemIcon>
-										<ListItemText primary='Signout' />
-									</ListItemButton>
-								</ListItem>
-							</List>
-					</Card>
-				</Stack>
-				) : (
-					<Stack spacing={2}>
-						<Stack
-							alignItems='center'
-							direction='row'
-							justifyContent='center'
-						>
-							<AvatarForm
-								onUpload={(url: string) => {
-									console.log(url);
-									// setAvatarUrl(url);
-									updateProfile({ full_name: profile.full_name, username: profile.username, avatar_url: url, color: profile.color });
-								}}
-								uid={user?.id ?? null}
-								url={avatar_url}
-							/>
-						</Stack>
-						<Card
-							elevation={0}
-							sx={{
-								backgroundColor: 'card.paper',
-							}}
-						>
-							<CardHeader
-								sx={{ textAlign: 'center' }}
-								title={username! || user?.email}
-							/>
-							<CardContent>
-								<Button
-									color='info'
-									onClick={handleCloseUpdate}
-									sx={{  mb: 2 }}
-								>
-									Close
-								</Button>
-								<AccountForm profile={profile} user={user} />
-							</CardContent>
-						</Card>
-					</Stack>
+				{(!openProfileUpdate&&!openProfileColor) && (
+					<Profile />
+				)}
+				{openProfileUpdate && (
+					<UpdateProfile />
+				)}
+				{openProfileColor && (
+					<UpdateColor />
 				)}
 				<Toolbar sx={{ mt: 6 }} />
 			</Main>
--- utils/context.tsx
+++ utils/context.tsx
@@ -3,29 +3,37 @@ import { createContext, useContext, useState } from 'react';
 import type { Todo } from './types';
 
 type InitialStateType = {
-	openTodoMenu: boolean,
 	openTodoAdd: boolean,
 	setOpenTodoAdd: React.Dispatch<boolean>,
 	openTodoEdit: boolean,
 	setOpenTodoEdit: React.Dispatch<boolean>,
-	anchor: HTMLElement|null,
-	setAnchor: React.Dispatch<HTMLElement|null>,
 	selectedTodo: Todo|null,
 	setSelectedTodo: React.Dispatch<Todo|null>,
+	openProfileUpdate: boolean,
+	setOpenProfileUpdate: React.Dispatch<boolean>,
+	openProfileColor: boolean,
+	setOpenProfileColor: React.Dispatch<boolean>,
+	anchor: HTMLElement|null,
+	setAnchor: React.Dispatch<HTMLElement|null>,
+	openTodoMenu: boolean,
 	handleOpenMenu: (event: React.MouseEvent<HTMLButtonElement>) => void,
 	handleCloseMenu: () => void,
 };
 
 const initialState = {
-	openTodoMenu: false,
 	openTodoAdd: false,
 	setOpenTodoAdd: () => null,
 	openTodoEdit: false,
 	setOpenTodoEdit: () => null,
-	anchor: null,
-	setAnchor: () => null,
 	selectedTodo: null,
 	setSelectedTodo: () => null,
+	openProfileUpdate: false,
+	setOpenProfileUpdate: () => null,
+	openProfileColor: false,
+	setOpenProfileColor: () => null,
+	anchor: null,
+	setAnchor: () => null,
+	openTodoMenu: false,
 	handleOpenMenu: () => null,
 	handleCloseMenu: () => null,
 };
@@ -39,8 +47,10 @@ export function OpenProvider({
 }>) {
 	const [openTodoAdd, setOpenTodoAdd] = useState(false);
 	const [openTodoEdit, setOpenTodoEdit] = useState(false);
-	const [anchor, setAnchor] = useState<HTMLElement|null>(null);
 	const [selectedTodo, setSelectedTodo] = useState<Todo|null>(null);
+	const [openProfileUpdate, setOpenProfileUpdate] = useState(false);
+	const [openProfileColor, setOpenProfileColor] = useState(false);
+	const [anchor, setAnchor] = useState<HTMLElement|null>(null);
   const openTodoMenu = Boolean(anchor);
 
   const handleOpenMenu = (event: React.MouseEvent<HTMLButtonElement>) => {
@@ -54,15 +64,19 @@ export function OpenProvider({
 	return (
 		<OpenContext.Provider
 			value={{
-				openTodoMenu,
 				openTodoAdd,
 				openTodoEdit,
 				setOpenTodoAdd,
 				setOpenTodoEdit,
-				anchor,
-				setAnchor,
 				selectedTodo,
 				setSelectedTodo,
+				openProfileUpdate,
+				setOpenProfileUpdate,
+				openProfileColor,
+				setOpenProfileColor,
+				anchor,
+				setAnchor,
+				openTodoMenu,
 				handleOpenMenu,
 				handleCloseMenu,
 			}}
